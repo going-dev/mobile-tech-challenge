5fa59d4f85e1930e42a8cc3e9cae69cb9cccd4fd
gretzky <sfed617@gmail.com>
2023-03-07 12:08:57 -0500

    chore(undefined): initial commit

--- apps/mobile/app/(app)/going.tsx
+++ apps/mobile/app/(app)/going.tsx
@@ -1,11 +1,8 @@
-import { Button, Spinner } from "native-base";
+import { Spinner } from "native-base";
 import { FlashList } from "@shopify/flash-list";
 import type { ReactElement } from "react";
-import { useCallback, useRef } from "react";
-import type BottomSheet from "@gorhom/bottom-sheet";
 import ScreenWrapper from "~/components/ScreenWrapper";
 import { useAddGoingCountries, useGoing, useMutateGoing } from "~/api/useGoing";
-import { CountriesSheet } from "~/components/CountriesSheet";
 import { CountryListItem } from "~/components/CountryListItem";
 import type { Country } from "~/types/country";
 
@@ -13,12 +10,6 @@ export default function Going(): ReactElement {
   const going = useGoing();
   const countries = useAddGoingCountries();
   const { mutate } = useMutateGoing();
-
-  const bottomSheetRef = useRef<BottomSheet>(null);
-
-  const handleSheetOpen = useCallback((): void => {
-    bottomSheetRef?.current?.snapToIndex(1);
-  }, []);
 
   return (
     <ScreenWrapper title="Where I'm Going">
@@ -40,12 +31,6 @@ export default function Going(): ReactElement {
               />
             )}
             estimatedItemSize={100}
-          />
-          <Button onPress={handleSheetOpen}>Add more</Button>
-          <CountriesSheet
-            ref={bottomSheetRef}
-            countries={countries?.data}
-            onPressCountry={(id: number): void => mutate({ id, isGoing: true })}
           />
         </>
       )}
--- apps/mobile/app/(app)/visited.tsx
+++ apps/mobile/app/(app)/visited.tsx
@@ -1,15 +1,12 @@
-import { Button, Spinner } from "native-base";
+import { Spinner } from "native-base";
 import { FlashList } from "@shopify/flash-list";
 import type { ReactElement } from "react";
-import { useCallback, useRef } from "react";
-import type BottomSheet from "@gorhom/bottom-sheet";
 import ScreenWrapper from "~/components/ScreenWrapper";
 import {
   useAddVisitedCountries,
   useVisited,
   useMutateVisited,
 } from "~/api/useVisited";
-import { CountriesSheet } from "~/components/CountriesSheet";
 import { CountryListItem } from "~/components/CountryListItem";
 import type { Country } from "~/types/country";
 
@@ -17,12 +14,6 @@ export default function Visited(): ReactElement {
   const visited = useVisited();
   const countries = useAddVisitedCountries();
   const { mutate } = useMutateVisited();
-
-  const bottomSheetRef = useRef<BottomSheet>(null);
-
-  const handleSheetOpen = useCallback((): void => {
-    bottomSheetRef?.current?.snapToIndex(1);
-  }, []);
 
   return (
     <ScreenWrapper title="Where I've Been">
@@ -44,16 +35,6 @@ export default function Visited(): ReactElement {
               />
             )}
             estimatedItemSize={100}
-          />
-          <Button mt="5" onPress={handleSheetOpen}>
-            Add more
-          </Button>
-          <CountriesSheet
-            ref={bottomSheetRef}
-            countries={countries?.data}
-            onPressCountry={(id: number): void =>
-              mutate({ id, isVisited: true })
-            }
           />
         </>
       )}
