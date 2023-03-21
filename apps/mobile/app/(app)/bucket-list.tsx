f81d6f762acb26ebcb33b453377afdb872ca3a90
gretzky <sfed617@gmail.com>
2023-03-21 08:09:18 -0400

    :construction: wip

--- apps/mobile/app/(app)/bucket-list.tsx
+++ apps/mobile/app/(app)/bucket-list.tsx
@@ -1,15 +1,12 @@
-import { Button, Spinner } from "native-base";
+import { Spinner } from "native-base";
 import { FlashList } from "@shopify/flash-list";
 import type { ReactElement } from "react";
-import { useCallback, useRef } from "react";
-import type BottomSheet from "@gorhom/bottom-sheet";
 import ScreenWrapper from "../../components/ScreenWrapper";
 import {
   useAddBucketListCountries,
   useBucketList,
   useMutateBucketList,
 } from "../../api/useBucketList";
-import { CountriesSheet } from "../../components/CountriesSheet";
 import { CountryListItem } from "../../components/CountryListItem";
 import type { Country } from "../../types/country";
 
@@ -17,12 +14,6 @@ export default function BucketList(): ReactElement {
   const bucketList = useBucketList();
   const countries = useAddBucketListCountries();
   const { mutate } = useMutateBucketList();
-
-  const bottomSheetRef = useRef<BottomSheet>(null);
-
-  const handleSheetOpen = useCallback((): void => {
-    bottomSheetRef?.current?.snapToIndex(1);
-  }, []);
 
   return (
     <ScreenWrapper title="Bucket List">
@@ -44,14 +35,6 @@ export default function BucketList(): ReactElement {
               />
             )}
             estimatedItemSize={100}
-          />
-          <Button onPress={handleSheetOpen}>Add more</Button>
-          <CountriesSheet
-            ref={bottomSheetRef}
-            countries={countries?.data}
-            onPressCountry={(_id: number): void =>
-              mutate({ _id, isBucketList: true })
-            }
           />
         </>
       )}
